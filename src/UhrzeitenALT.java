26efbe554eed55e30e47b43e7ee6efd2471c6e2f
evainga <ego87@web.de>
2016-10-15 16:17:22 +0200

    Uhrzeiten OLD
    One Step back in order ro compare UhrzeitenOld to Uhrzeiten,java

---
+++ src/UhrzeitenALT.java
@@ -0,0 +1,15 @@
+public final class UhrzeitenALT {
+	public static void main(final java.lang.String[] args) {
+		int stunden;
+		int minuten;
+		for (stunden = 0; stunden < 24; stunden++) {
+			for (minuten = 0; minuten < 60; minuten++) {
+				System.out.print(stunden + ".");
+				if (minuten < 10) {
+					System.out.print("0");
+				}
+				System.out.println(minuten);
+			}
+		}
+	}
+}
