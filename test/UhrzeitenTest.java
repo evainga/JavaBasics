1d31ebf73b875fed7bc22f3214d0e91c7d04f8f7
evainga <ego87@web.de>
2016-10-24 21:53:47 +0200

    homework task 1

--- test/UhrzeitenTest.java
+++ test/UhrzeitenTest.java
@@ -1,7 +1,9 @@
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.lessThan;
 
 import java.util.List;
 
@@ -15,7 +17,9 @@ public class UhrzeitenTest {
 		List<String> times = uhrzeiten.assembleAllValidTimes();
 		assertThat(times, hasSize(1440));
 		assertThat(times.get(2), is("0.02"));
-		assertThat(times, not(hasSize(1440)));
+		assertThat(times, not(hasSize(1220)));
+		assertThat(times.size(), greaterThan(1000));
+		assertThat(times.size(), lessThan(2000));
 	}
 
 	@Test(expected = IndexOutOfBoundsException.class)
