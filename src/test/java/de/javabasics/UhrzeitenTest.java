1e5bb54351538fc9e1604054e5804df3779972e8
evainga <ego87@web.de>
2016-10-30 13:52:08 +0100

    #6 solve homework task 2 and try 3+4

--- src/test/java/de/javabasics/UhrzeitenTest.java
+++ src/test/java/de/javabasics/UhrzeitenTest.java
@@ -1,7 +1,10 @@
 package de.javabasics;
 
+import static org.hamcrest.CoreMatchers.both;
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.startsWith;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.hasSize;
@@ -20,13 +23,11 @@ public class UhrzeitenTest {
 		assertThat(times, hasSize(1440));
 		assertThat(times.get(2), is("0.02"));
 		assertThat(times, not(hasSize(1220)));
-		assertThat(times.size(), greaterThan(1000));
-		assertThat(times.size(), lessThan(2000));
-
+		assertThat(times.size(), both(lessThan(2000)).and(greaterThan(1000)));
 	}
 
 	@Test(expectedExceptions = IndexOutOfBoundsException.class)
-	public void testIndexOutOfBoundsException() {
+	public void testLowerBounds() {
 		Uhrzeiten uhrzeiten = new Uhrzeiten();
 		List<String> times = uhrzeiten.assembleAllValidTimes();
 		times.get(-1);
@@ -38,4 +39,13 @@ public class UhrzeitenTest {
 		List<String> times = uhrzeiten.assembleAllValidTimes();
 		times.get(2000);
 	}
+
+	@Test
+	public void testStrings() {
+		Uhrzeiten uhrzeiten = new Uhrzeiten();
+		List<String> times = uhrzeiten.assembleAllValidTimes();
+		assertThat(times.get(3), containsString("."));
+		assertThat(times.get(3), startsWith("0.0"));
+	}
+
 }
