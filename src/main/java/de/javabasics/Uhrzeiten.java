e38ae5a8ceb8bbcefbdc5e3c859cd5afe40c8d39
evainga <ego87@web.de>
2016-10-31 22:17:46 +0100

    # fix Uhrzeiten.java

--- src/main/java/de/javabasics/Uhrzeiten.java
+++ src/main/java/de/javabasics/Uhrzeiten.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class Uhrzeiten {
 	private static final String MINUTE_HOUR_SEPARATOR = ":";
+
 	public List<String> assembleAllValidTimes() {
 		List<String> times = new ArrayList<>();
 		int stunden;
@@ -14,7 +15,7 @@ public class Uhrzeiten {
 				String assembledString = stunden + MINUTE_HOUR_SEPARATOR;
 				if (minuten < 10)
 					assembledString = assembledString + 0;
-				assembledString = assembledString + minuten;
+				assembledString = assembledString + minuten + " Uhr";
 				times.add(assembledString);
 			}
 		}
--- src/test/java/de/javabasics/UhrzeitenTest.java
+++ src/test/java/de/javabasics/UhrzeitenTest.java
@@ -2,10 +2,10 @@ package de.javabasics;
 
 import static org.hamcrest.CoreMatchers.both;
 import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.endsWith;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.CoreMatchers.startsWith;
-import static org.hamcrest.CoreMatchers.endsWith;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.hasSize;
@@ -23,7 +23,7 @@ public class UhrzeitenTest {
 	public void assembleAllValidTimes() {
 		List<String> times = uhrzeiten.assembleAllValidTimes();
 		assertThat(times, hasSize(1440));
-		assertThat(times.get(2), is("0:02"));
+		assertThat(times.get(2), is("0:02 Uhr"));
 		assertThat(times, not(hasSize(1220)));
 		assertThat(times.size(), both(lessThan(2000)).and(greaterThan(1000)));
 	}
@@ -46,38 +46,37 @@ public class UhrzeitenTest {
 		assertThat(times.get(3), containsString(":"));
 		assertThat(times.get(3), startsWith("0:0"));
 	}
-	
+
 	@Test
 	public void precedingZerosWithLambda() {
 		List<String> times = uhrzeiten.assembleAllValidTimes();
-		
-		int minutesWithPrecedingZeros = times.stream()
-			.filter(time -> isPrecedingZeroInMinutePart(time))
-			.collect(Collectors.toList()).size();
-		
+
+		int minutesWithPrecedingZeros = times.stream().filter(time -> isPrecedingZeroInMinutePart(time))
+				.collect(Collectors.toList()).size();
+
 		assertThat(minutesWithPrecedingZeros, is(10 * 24));
 	}
 
 	@Test
 	public void precedingZerosWithForEachLoop() {
 		List<String> times = uhrzeiten.assembleAllValidTimes();
-		
+
 		int minutesWithPrecedingZeros = 0;
 		for (String time : times)
 			if (isPrecedingZeroInMinutePart(time))
 				minutesWithPrecedingZeros++;
-		
+
 		assertThat(minutesWithPrecedingZeros, is(10 * 24));
 	}
-	
+
 	private boolean isPrecedingZeroInMinutePart(String time) {
 		return time.split(":")[1].startsWith("0");
 	}
-	
+
 	@Test
 	public void allTimesHaveUhrSuffix() {
 		List<String> times = uhrzeiten.assembleAllValidTimes();
-		
+
 		for (String time : times)
 			assertThat(time, endsWith(" Uhr"));
 	}
